c4bd96fb335de180f6a89df9c0df9fd2696fd959
Alexander Er <aceszhenwei@yahoo.com>
2022-03-11 18:20:41 +0800

    Update LogicManagerTest.java
    Corrected a mispelling within LogicManagerTest from 'Person' to 'Client'

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -59,7 +59,7 @@ public class LogicManagerTest {
     @Test
     public void execute_commandExecutionError_throwsCommandException() {
         String deleteCommand = "deleteClient 9";
-        assertCommandException(deleteCommand, MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertCommandException(deleteCommand, MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX);
     }
 
     @Test
