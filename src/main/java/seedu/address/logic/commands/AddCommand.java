fef78b0804518764cf468cac4b7f4a7dd01d41cf
jbkim1999 <jungbae.kim@u.nus.edu>
2022-03-12 16:09:30 +0800

    Fix AddressBookParserTest

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -25,6 +25,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
+            + PREFIX_PLAN + "PLAN "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
