b919d9f9cc4d52af0cea778e7a2c7ffc679defef
jbkim1999 <jungbae.kim@u.nus.edu>
2022-03-13 20:49:16 +0800

    Remove unnecessary imports from AddProcCommand

--- src/main/java/seedu/address/logic/commands/AddProcCommand.java
+++ src/main/java/seedu/address/logic/commands/AddProcCommand.java
@@ -1,15 +1,9 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COST;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INFORMATION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CLIENTS;
 
 import java.util.List;
 
