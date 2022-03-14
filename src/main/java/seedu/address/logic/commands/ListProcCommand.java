254c2af6f39a99f05d84be69ab42408b1418a851
Yong Gi <rygrobin@gmail.com>
2022-03-14 20:14:50 +0800

    Add ListProcCommand and ListProcCommandParser and some corresponding tests

--- src/main/java/seedu/address/logic/commands/ListProcCommand.java
+++ src/main/java/seedu/address/logic/commands/ListProcCommand.java
@@ -1,8 +1,12 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.client.Client;
+
+import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.*;
@@ -13,33 +17,39 @@ public class ListProcCommand extends Command {
     public static final String COMMAND_WORD = "listProc";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists the procedures of a client in the index. "
-            + "Parameters: INDEX"
+            + "Parameters: INDEX (must be a positive integer) \n"
             + "Example: listProc 1";
 
     public static final String MESSAGE_SUCCESS = "Procedures successfully loaded.";
 
-    private final Client target;
+    private final Index targetIndex;
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        List<Client> lastShownList = model.getFilteredClientList();
 
-        model.addClient(target);
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX);
+        }
+        Client clientToShow = lastShownList.get(targetIndex.getZeroBased());
+        System.out.println(clientToShow.toString());
+        // TODO: get list of procedures and
         return new CommandResult(MESSAGE_SUCCESS);
     }
 
     /**
      * Creates an ListProcCommand to add the specified {@code Client}
      */
-    public ListProcCommand(Client target) {
-        requireNonNull(target);
-        this.target = target;
+    public ListProcCommand(Index targetIndex) {
+        requireNonNull(targetIndex);
+        this.targetIndex = targetIndex;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof ListProcCommand // instanceof handles nulls
-                && target.equals(((ListProcCommand) other).target));
+                && targetIndex.equals(((ListProcCommand) other).targetIndex));
     }
 }
---
+++ src/test/java/seedu/address/logic/parser/ListProcCommandParserTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.ListProcCommand;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
+
+public class ListProcCommandParserTest {
+    private ListProcCommandParser parser = new ListProcCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsListProcCommand() {
+        assertParseSuccess(parser, "1", new ListProcCommand(INDEX_FIRST_CLIENT));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "b", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ListProcCommand.MESSAGE_USAGE));
+    }
+}
