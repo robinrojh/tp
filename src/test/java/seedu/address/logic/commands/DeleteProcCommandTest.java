a56dd4959a713004dfe7cdddb7cb97330e654935
teominghuang <bennyteomhh@hotmail.com>
2022-03-23 11:52:22 +0800

    Add Procedure Test

--- src/test/java/seedu/address/logic/commands/DeleteProcCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteProcCommandTest.java
@@ -1,10 +1,22 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalClients.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PROCEDURE;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.client.Client;
+import seedu.address.model.procedure.Procedure;
+import seedu.address.testutil.ClientBuilder;
+import seedu.address.testutil.ProcedureBuilder;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,17 +25,23 @@ import seedu.address.model.UserPrefs;
 public class DeleteProcCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    //Will do up more in due time!
-    //@Test
-    //public void execute_procedureWasDeletedFromClient_success() {
-    //Client editedClient = new ClientBuilder().build();
-    //DeleteProcCommand deleteCommand = new DeleteProcCommand(INDEX_FIRST_CLIENT, INDEX_FIRST_PROCEDURE);
 
-    //String expectedMessage = String.format(DeleteProcCommand.MESSAGE_EDIT_CLIENT_SUCCESS,
-    // editedClient.getProcedures());
-    //Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-    //expectedModel.setClient(model.getFilteredClientList().get(0), editedClient);
+    @Test
+    public void execute_procedureDeletedFromExistingClient_success() {
 
-    //assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
-    //}
+        Procedure validProcedure = new ProcedureBuilder().build();
+        ArrayList<Procedure> validProcedureList = new ArrayList<>();
+        validProcedureList.add(validProcedure);
+
+        Client validClient = new ClientBuilder().withProcedures(validProcedureList).build();
+        //Client clientToDeleteProc = model.getFilteredClientList().get(0);
+        DeleteProcCommand deleteProcCommand = new DeleteProcCommand(INDEX_FIRST_CLIENT, INDEX_FIRST_PROCEDURE);
+
+        String expectedMessage = String.format(DeleteProcCommand
+                .MESSAGE_DELETE_PROCEDURE_SUCCESS, validProcedure);
+
+        ModelManager expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+
+        assertCommandSuccess(deleteProcCommand, model, expectedMessage, expectedModel);
+    }
 }
