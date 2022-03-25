97f32388adaf3d16cf01f2268ad9b40af705ac47
Alexander Er <aceszhenwei@yahoo.com>
2022-03-25 15:19:19 +0800

    Fixed Checkstyle Issues

--- src/test/java/seedu/address/logic/commands/AddProcCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddProcCommandTest.java
@@ -1,7 +1,8 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_REPAIR;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_REPLACE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_COST_REPAIR;
@@ -15,38 +16,39 @@ import static seedu.address.testutil.TypicalClients.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_CLIENT;
 
-import javafx.collections.ObservableList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.*;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.client.Client;
-import seedu.address.model.procedure.Information;
 import seedu.address.model.procedure.Procedure;
 import seedu.address.testutil.ClientBuilder;
 import seedu.address.testutil.ProcedureBuilder;
 import seedu.address.testutil.RandomProcedureBuilder;
 
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-import java.util.function.Predicate;
+
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for AddProcCommand.
  */
 public class AddProcCommandTest {
 
+    private static final int EXPECTED_LENGTH_OF_LIST = 3;
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     // new Model without any Clients
     private Model newModel = new ModelManager();
     private Procedure toBeAddedProcedure = new ProcedureBuilder().build();
-    private static final int EXPECTED_LENGTH_OF_LIST = 3;
+
 
     @Test
     public void execute_properIndexWithProperProcedure_success() {
@@ -83,15 +85,11 @@ public class AddProcCommandTest {
             Procedure randomProcedure = randomProcedureBuilder.buildRandomProcedure(i);
             currentProcList.add(randomProcedure);
             expectedProcList.add(randomProcedure);
-        }
-
-        Collections.shuffle(currentProcList);
-
+        } Collections.shuffle(currentProcList);
         try {
             List<Procedure> newProcList = addProcCommand.procListWithAddedProc(currentProcList);
             assertEquals(expectedProcList, newProcList);
-        }
-        catch (CommandException e) {
+        } catch (CommandException e) {
             assert false;
         }
     }
--- src/test/java/seedu/address/testutil/TypicalRandomProcedureDetails.java
+++ src/test/java/seedu/address/testutil/TypicalRandomProcedureDetails.java
@@ -1,9 +1,10 @@
 package seedu.address.testutil;
 
+import java.util.Random;
+
 import seedu.address.model.procedure.Cost;
 import seedu.address.model.procedure.Date;
 import seedu.address.model.procedure.Information;
-import java.util.Random;
 
 /**
  * Holds typical details that can make up a Procedure. Date field has 3 additional separate components to account for
@@ -14,24 +15,18 @@ import java.util.Random;
 public class TypicalRandomProcedureDetails {
 
     public static final Information[] EXAMPLE_INFORMATION = {
-            new Information("Apple Inc."), new Information("Google Inc."), new Information("Singtel Inc.")
+        new Information("Apple Inc."), new Information("Google Inc."), new Information("Singtel Inc.")
     };
     public static final Cost[] EXAMPLE_COST = {
-            new Cost("10.00"), new Cost("23.23"), new Cost("76.10")
+        new Cost("10.00"), new Cost("23.23"), new Cost("76.10")
     };
     public static final Date[][] EXAMPLE_DATE_SEPARATED_BY_PERIOD = {
-            {
-                new Date("29/02/2012 10:00"), new Date("10/10/2015 13:45"), new Date("02/03/2013 09:00")
-            },
-            {
-                new Date("25/12/2015 16:00"), new Date("01/02/2016 17:12"), new Date("03/11/2018 12:00")
-            },
-            {
-                new Date("12/12/2018 09:00"), new Date("29/02/2020 11:10"), new Date("03/03/2022 11:01")
-            }
+            {new Date("29/02/2012 10:00"), new Date("10/10/2015 13:45"), new Date("02/03/2013 09:00")},
+            {new Date("25/12/2015 16:00"), new Date("01/02/2016 17:12"), new Date("03/11/2018 12:00")},
+            {new Date("12/12/2018 09:00"), new Date("29/02/2020 11:10"), new Date("03/03/2022 11:01")}
     };
 
-    public static Random rand = new Random();
+    private static Random rand = new Random();
     private static final Date EARLY_DATE = EXAMPLE_DATE_SEPARATED_BY_PERIOD[0][rand.nextInt(3)];
     private static final Date NEUTRAL_DATE = EXAMPLE_DATE_SEPARATED_BY_PERIOD[1][rand.nextInt(3)];
     private static final Date FUTURE_DATE = EXAMPLE_DATE_SEPARATED_BY_PERIOD[2][rand.nextInt(3)];
@@ -47,6 +42,10 @@ public class TypicalRandomProcedureDetails {
     Cost getCost() {
         int randInt = rand.nextInt(3);
         return EXAMPLE_COST[randInt];
+    }
+
+    Random getRand() {
+        return rand;
     }
 
     /**
