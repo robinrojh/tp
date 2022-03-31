4ab663b66622f27fa4a72fa6b0698de0bd7e3c28
Yong Gi <rygrobin@gmail.com>
2022-03-31 22:15:04 +0800

    Update find command to display procedures correctly

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -5,6 +5,10 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.client.NameContainsKeywordsPredicate;
+import seedu.address.model.procedure.Procedure;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Finds and lists all clients in address book whose name contains any of the argument keywords.
@@ -29,6 +33,12 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredClientList(predicate);
+        if (model.getFilteredClientList().size() > 0) {
+            model.updateFilteredProcedureList(model.getFilteredClientList().get(0),
+                    Model.PREDICATE_SHOW_CLIENT_PROCEDURES);
+        } else {
+            model.setProcedures(new ArrayList<Procedure>());
+        }
         return new CommandResult(
                 String.format(Messages.MESSAGE_CLIENTS_LISTED_OVERVIEW, model.getFilteredClientList().size()));
     }
