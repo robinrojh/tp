8d601708c0850e9237cb68ff8058bb279bc2bfb3
teominghuang <bennyteomhh@hotmail.com>
2022-04-01 07:21:03 +0800

    Fix checkstyle issue

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -1,15 +1,13 @@
 package seedu.address.commons.core;
 
-import seedu.address.logic.commands.DeleteProcCommand;
-
 /**
  * Container for user visible messages.
  */
 public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "Please provide a Client index that " +
-        "belongs to an existing Client.";
+    public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "Please provide a Client index that "
+        + "belongs to an existing Client.";
     public static final String MESSAGE_INVALID_CLIENT_NON_POSITIVE_INDEX = "Please provide a positive client "
             + "index";
     public static final String MESSAGE_INVALID_PROCEDURE_DISPLAYED_INDEX =
