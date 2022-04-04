64bab14e08b47c8e6037936899447a862e64eb81
teominghuang <bennyteomhh@hotmail.com>
2022-04-04 17:40:16 +0800

    Fix checkstyle issue

--- src/test/java/seedu/address/logic/commands/EditProcCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditProcCommandTest.java
@@ -1,6 +1,22 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_COST_REPAIR;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_COST_REPLACE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_REPAIR;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_REPLACE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_INFO_REPAIR;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_INFO_REPLACE;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalClients.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PROCEDURE;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.EditProcCommand.EditProcedureDescriptor;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -11,88 +27,80 @@ import seedu.address.testutil.ClientBuilder;
 import seedu.address.testutil.EditProcedureDescriptorBuilder;
 import seedu.address.testutil.ProcedureBuilder;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-
-import seedu.address.logic.commands.EditProcCommand.EditProcedureDescriptor;
-
-import static seedu.address.logic.commands.CommandTestUtil.*;
-import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalClients.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalIndexes.*;
-
 /**
  * Contains integration tests (interaction with the Model) and unit tests for EditProcCommand.
  */
 public class EditProcCommandTest {
-	private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-	@Test
-	public void execute_editProcedureFromExistingClient_success() {
-		Procedure procedureToEdit = new ProcedureBuilder().withInfo(VALID_INFO_REPAIR).
-				withCost(VALID_COST_REPAIR).withDate(VALID_DATE_REPAIR).build();
-		ArrayList<Procedure> clientProcedures = new ArrayList<>();
-		clientProcedures.add(procedureToEdit);
+    @Test
+    public void execute_editProcedureFromExistingClient_success() {
+        Procedure procedureToEdit = new ProcedureBuilder().withInfo(VALID_INFO_REPAIR)
+                .withCost(VALID_COST_REPAIR).withDate(VALID_DATE_REPAIR).build();
+        ArrayList<Procedure> clientProcedures = new ArrayList<>();
+        clientProcedures.add(procedureToEdit);
+        if (model.getFilteredClientList().size() < 1) {
+            Client validClient = new ClientBuilder().withProcedures(clientProcedures).build();
+            model.addClient(validClient);
+        } else {
+            Client editThisClient = model.getFilteredClientList().get(INDEX_FIRST_CLIENT.getZeroBased());
+            Client validClient = new ClientBuilder(editThisClient).withProcedures(clientProcedures).build();
+            model.setClient(editThisClient, validClient);
+        }
 
-		if (model.getFilteredClientList().size() < 1){
-			Client validClient = new ClientBuilder().withProcedures(clientProcedures).build();
-			model.addClient(validClient);
-		} else {
-			Client editThisClient = model.getFilteredClientList().get(INDEX_FIRST_CLIENT.getZeroBased());
-			Client validClient = new ClientBuilder(editThisClient).withProcedures(clientProcedures).build();
-			model.setClient(editThisClient, validClient);
-		}
+        ModelManager expectedModel = new ModelManager(new AddressBook(model.getAddressBook()),
+                new UserPrefs());
+        Procedure newProcedure = new ProcedureBuilder().withInfo(VALID_INFO_REPLACE)
+                .withCost(VALID_COST_REPLACE).withDate(VALID_DATE_REPLACE).build();
+        EditProcedureDescriptor editProcedureDescriptor = new EditProcedureDescriptorBuilder(newProcedure)
+                .build();
+        EditProcCommand editProcCommand = new EditProcCommand(INDEX_FIRST_CLIENT,
+                INDEX_FIRST_PROCEDURE, editProcedureDescriptor);
 
-		ModelManager expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-		Procedure newProcedure = new ProcedureBuilder().withInfo(VALID_INFO_REPLACE).
-			withCost(VALID_COST_REPLACE).withDate(VALID_DATE_REPLACE).build();
-		EditProcedureDescriptor editProcedureDescriptor = new EditProcedureDescriptorBuilder(newProcedure).build();
-		EditProcCommand editProcCommand = new EditProcCommand(INDEX_FIRST_CLIENT, INDEX_FIRST_PROCEDURE, editProcedureDescriptor);
+        Client targetClient = expectedModel.getFilteredClientList().get(0);
+        ArrayList<Procedure> newProcedures = new ArrayList<>(targetClient.getProcedures());
+        newProcedures.set(INDEX_FIRST_PROCEDURE.getZeroBased(), newProcedure);
+        Client editedClient = new ClientBuilder(targetClient).withProcedures(newProcedures).build();
+        expectedModel.setClient(targetClient, editedClient);
+        String expectedMessage = String.format(EditProcCommand.MESSAGE_EDIT_PROCEDURE_SUCCESS, newProcedure,
+                editedClient.getName(), editedClient.getAddress());
+        assertCommandSuccess(editProcCommand, model, expectedMessage, expectedModel);
+    }
 
-		Client targetClient = expectedModel.getFilteredClientList().get(0);
-		ArrayList<Procedure> newProcedures = new ArrayList<>(targetClient.getProcedures());
-		newProcedures.set(INDEX_FIRST_PROCEDURE.getZeroBased(), newProcedure);
-		Client editedClient = new ClientBuilder(targetClient).withProcedures(newProcedures).build();
-		expectedModel.setClient(targetClient, editedClient);
-		String expectedMessage = String.format(EditProcCommand.MESSAGE_EDIT_PROCEDURE_SUCCESS, newProcedure, editedClient.getName(), editedClient.getAddress());
-		assertCommandSuccess(editProcCommand, model, expectedMessage, expectedModel);
-	}
+    @Test
+    public void execute_editProcedureFromNullClient_failure() {
+        assertThrows(NullPointerException.class, () -> new EditProcCommand(null,
+            INDEX_FIRST_PROCEDURE, new EditProcedureDescriptorBuilder(new ProcedureBuilder().build())
+            .build()));
+    }
 
-	@Test
-	public void execute_editProcedureFromNullClient_failure() {
-		assertThrows(NullPointerException.class, () -> new EditProcCommand(null,
-			INDEX_FIRST_PROCEDURE, new EditProcedureDescriptorBuilder(new ProcedureBuilder().build())
-			.build()));
-	}
+    @Test
+    public void execute_editProcedureFromNullProcedure_failure() {
+        ArrayList<Procedure> clientProcedures = new ArrayList<>();
+        Client newClient = new ClientBuilder().withProcedures(clientProcedures).build();
+        if (model.getFilteredClientList().size() < 1) {
+            model.addClient(newClient);
+        } else {
+            model.setClient(model.getFilteredClientList().get(0), newClient);
+        }
+        assertThrows(NullPointerException.class, () -> new EditProcCommand(null,
+            INDEX_FIRST_PROCEDURE, new EditProcedureDescriptorBuilder(new ProcedureBuilder().build())
+            .build()));
+    }
 
-	@Test
-	public void execute_editProcedureFromNullProcedure_failure() {
-		ArrayList<Procedure> clientProcedures = new ArrayList<>();
-		Client newClient = new ClientBuilder().withProcedures(clientProcedures).build();
-		if (model.getFilteredClientList().size() < 1){
-			model.addClient(newClient);
-		} else {
-			model.setClient(model.getFilteredClientList().get(0), newClient);
-		}
-		assertThrows(NullPointerException.class, () -> new EditProcCommand(null,
-			INDEX_FIRST_PROCEDURE, new EditProcedureDescriptorBuilder(new ProcedureBuilder().build())
-			.build()));
-	}
+    @Test
+    public void execute_editProcedureFromNullDescriptor_failure() {
+        Procedure newProcedure = new ProcedureBuilder().build();
+        ArrayList<Procedure> clientProcedures = new ArrayList<>();
+        clientProcedures.add(newProcedure);
+        Client newClient = new ClientBuilder().withProcedures(clientProcedures).build();
+        if (model.getFilteredClientList().size() < 1) {
+            model.addClient(newClient);
+        } else {
+            model.setClient(model.getFilteredClientList().get(0), newClient);
+        }
 
-	@Test
-	public void execute_editProcedureFromNullDescriptor_failure() {
-		Procedure newProcedure = new ProcedureBuilder().build();
-		ArrayList<Procedure> clientProcedures = new ArrayList<>();
-		clientProcedures.add(newProcedure);
-		Client newClient = new ClientBuilder().withProcedures(clientProcedures).build();
-		if (model.getFilteredClientList().size() < 1){
-			model.addClient(newClient);
-		} else {
-			model.setClient(model.getFilteredClientList().get(0), newClient);
-		}
-
-		assertThrows(NullPointerException.class, () -> new EditProcCommand(INDEX_FIRST_CLIENT,
-			INDEX_FIRST_PROCEDURE, null));
-	}
+        assertThrows(NullPointerException.class, () -> new EditProcCommand(INDEX_FIRST_CLIENT,
+            INDEX_FIRST_PROCEDURE, null));
+    }
 }
--- src/test/java/seedu/address/testutil/EditProcedureDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditProcedureDescriptorBuilder.java
@@ -11,51 +11,51 @@ import seedu.address.model.procedure.Procedure;
  */
 public class EditProcedureDescriptorBuilder {
 
-	private EditProcedureDescriptor descriptor;
+    private EditProcedureDescriptor descriptor;
 
-	public EditProcedureDescriptorBuilder() {
-		descriptor = new EditProcedureDescriptor();
-	}
+    public EditProcedureDescriptorBuilder() {
+        descriptor = new EditProcedureDescriptor();
+    }
 
-	public EditProcedureDescriptorBuilder(EditProcedureDescriptor descriptor) {
-		this.descriptor = new EditProcedureDescriptor(descriptor);
-	}
+    public EditProcedureDescriptorBuilder(EditProcedureDescriptor descriptor) {
+        this.descriptor = new EditProcedureDescriptor(descriptor);
+    }
 
-	/**
-	 * Returns an {@code EditProcedureDescriptorBuilder} with fields containing {@code Procedure}'s details
-	 */
-	public EditProcedureDescriptorBuilder(Procedure procedure) {
-		descriptor = new EditProcedureDescriptor();
-		descriptor.setInfo(procedure.getInfo());
-		descriptor.setDate(procedure.getDate());
-		descriptor.setCost(procedure.getCost());
-	}
+    /**
+     * Returns an {@code EditProcedureDescriptorBuilder} with fields containing {@code Procedure}'s details
+     */
+    public EditProcedureDescriptorBuilder(Procedure procedure) {
+        descriptor = new EditProcedureDescriptor();
+        descriptor.setInfo(procedure.getInfo());
+        descriptor.setDate(procedure.getDate());
+        descriptor.setCost(procedure.getCost());
+    }
 
-	/**
-	 * Sets the {@code Information} of the {@code EditProcedureDescriptor} that we are building.
-	 */
-	public EditProcedureDescriptorBuilder withInfo(String info) {
-		descriptor.setInfo(new Information(info));
-		return this;
-	}
+    /**
+     * Sets the {@code Information} of the {@code EditProcedureDescriptor} that we are building.
+     */
+    public EditProcedureDescriptorBuilder withInfo(String info) {
+        descriptor.setInfo(new Information(info));
+        return this;
+    }
 
-	/**
-	 * Sets the {@code Date} of the {@code EditProcedureDescriptor} that we are building.
-	 */
-	public EditProcedureDescriptorBuilder withDate(String date) {
-		descriptor.setDate(new Date(date));
-		return this;
-	}
+    /**
+     * Sets the {@code Date} of the {@code EditProcedureDescriptor} that we are building.
+     */
+    public EditProcedureDescriptorBuilder withDate(String date) {
+        descriptor.setDate(new Date(date));
+        return this;
+    }
 
-	/**
-	 * Sets the {@code Cost} of the {@code EditProcedureDescriptor} that we are building.
-	 */
-	public EditProcedureDescriptorBuilder withCost(String cost) {
-		descriptor.setCost(new Cost(cost));
-		return this;
-	}
+    /**
+     * Sets the {@code Cost} of the {@code EditProcedureDescriptor} that we are building.
+     */
+    public EditProcedureDescriptorBuilder withCost(String cost) {
+        descriptor.setCost(new Cost(cost));
+        return this;
+    }
 
-	public EditProcedureDescriptor build() {
-		return descriptor;
-	}
+    public EditProcedureDescriptor build() {
+        return descriptor;
+    }
 }
